38c168acf66f59d234ed578c40e5a2de2a913fc3
masoudmasoud <msimasoud@gmail.com>
2017-01-25 15:26:09 +0330

    first commit
    6/11/95

---
+++ resources/lang/ar/admin/locations/table.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'about_locations_title'     => 'درباره مکان ها',
+    'about_locations'           => 'مکان استفاده می شود برای ردیابی اطلاعات مکان برای کاربران، دارایی ها، و موارد دیگر',
+    'assets_rtd'                => 'دارایی ها', // This has NEVER meant Assets Retired. I don't know how it keeps getting reverted.
+    'assets_checkedout'         => 'دارایی واگذار شده
+',
+    'id'                        => 'شناسه',
+    'city'                      => 'شهر',
+    'state'                     => 'استان',
+    'country'                   => 'كشور',
+    'create'                    => 'ساخت مکان',
+    'update'                    => 'به روز رسانی منطقه مکانی',
+    'name'                      => 'نام منطقه مکانی',
+    'address'                   => 'آدرس',
+    'zip'                       => 'کد پستی',
+    'locations'                 => 'مکانها',
+    'parent'                    => 'مجموعه پدر',
+    'currency'                  => 'مکان ارز',
+    );
---
+++ resources/lang/ar/admin/users/general.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return array(
+
+    'assets_user'       => 'دارایی ها محول شده به :',
+    'current_assets'    => 'دارایی های اکنون  برای این کاربر بیرون کشیده شد',
+    'clone'             => 'کلون کاربر',
+    'contact_user'      => 'تماس با:',
+    'edit'              => 'ویرایش کاربر',
+    'filetype_info'     => 'پسوند های مجاز:png, gif, jpg, jpeg, doc, docx, pdf, txt, zip,Rar',
+    'history_user'      => 'تاریخچه برای:',
+    'last_login'        => 'آخرین ورود',
+    'ldap_config_text'  => 'تنظشیمات تاییدیه LDPA در فولدر app/config در فایلی به نام idap.php وجود دارد.مکان مشخص شده برای تمامی کاربران وارد شده تنظیم میشود.شما نیاز دارید که حداقل یه مکان را اضافه کرده باشید تا بتوانید از این ویژگی استفاده کنید.',
+    'software_user'     => 'برنامه چک شد برای:',
+    'view_user'         => 'نمایش کاربر :',
+    'usercsv'           => 'فایل CSV',
+    'two_factor_admin_optin_help' => 'تنظیمات مدیریتی فعلی مجوز اجرای انتخابی از سیستم احراز هویت Two-Factor را می دهد.  ',
+    'two_factor_enrolled' => 'سیستم Two-Factor ثبت نام شده',
+    'two_factor_active'   => '2FA فعال ',
+    );
